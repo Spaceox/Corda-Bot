caf83396d1b8f2a9e8e0485d8b8af9360e0a83ac
Spaceox <91277246+Spaceox@users.noreply.github.com>
2021-10-01 23:00:51 +0200

    Sistemata una cavolata
    o cazzata, stessa roba.

--- commands/misc/uptime.js
+++ commands/misc/uptime.js
@@ -6,23 +6,28 @@ module.exports = {
     .setName('uptime')
     .setDescription("Risponde con l'uptime!"),
   async execute(interaction) {
-    // sec => HHMMSS
-    const uptime = new Date(interaction.client.uptime);
-    const UPh = uptime.getUTCHours();
-    const UPmin = uptime.getUTCMinutes();
-    const UPsec = uptime.getSeconds();
-    const timeString =
-      UPh.toString().padStart(2, '0') +
-      ':' +
-      UPmin.toString().padStart(2, '0') +
-      ':' +
-      UPsec.toString().padStart(2, '0');
+    // sec => HHMMSS https://stackoverflow.com/questions/28705009/how-do-i-get-the-server-uptime-in-node-js
+    function format(seconds) {
+      function pad(s) {
+        return (s < 10 ? '0' : '') + s;
+      }
+      var hours = Math.floor(seconds / (60 * 60));
+      var minutes = Math.floor((seconds % (60 * 60)) / 60);
+      var seconds = Math.floor(seconds % 60);
+
+      return pad(hours) + ':' + pad(minutes) + ':' + pad(seconds);
+    }
+    // ms => sec https://stackoverflow.com/questions/21294302/converting-milliseconds-to-minutes-and-seconds-with-javascript
+    var millis = ((interaction.client.uptime % 60000) / 1000).toFixed(0);
+    const timed = format(millis);
+    console.log(timed);
+
     // Carica l'uptime in un embed
     const pinged = new MessageEmbed()
       .setTitle(`:clock1230:  Uptime!`)
       .setColor('BLURPLE')
       .setDescription(
-        `:white_check_mark:  **${interaction.client.user.username}** è attivo da: ${timeString}`
+        `:white_check_mark:  **${interaction.client.user.username}** è attivo da: ${timed}`
       );
     // e lo invia
     await interaction.reply({ embeds: [pinged] });
