b29c216c9afd474d179aa859ad3ccce42ea89c77
Spaceox <91277246+Spaceox@users.noreply.github.com>
2021-10-09 23:29:59 +0200

    Test1

--- events/ready.js
+++ events/ready.js
@@ -4,9 +4,7 @@ module.exports = {
   execute(client) {
     // Messaggio di login + RichPresence e Stato
     console.log(`Eseguito il login come ${client.user.tag}.`);
-    client.user.setPresence({
-      activities: [{ name: 'Power, Super Power' }],
-    });
+    client.user.setActivity('Netflix', { type: 'WATCHING' });
     client.user.setStatus('dnd');
   },
 };
